40935c94c16ee80463da48c72a3e429dd0e5536f
2Lalo21 <130114774+2Lalo21@users.noreply.github.com>
2023-05-22 21:44:54 -0500

    si eso

--- db/migrations.py
+++ db/migrations.py
@@ -1,7 +1,7 @@
 import sqlite3
 def create_table_cliente(*args,**kwargs):
     connection = kwargs.pop('connection')
-    cursor = connection.cursor()
+    conn = kwargs.pop("database.db")
     query  = '''
     CREATE TABLE IF NOT EXISTS cliente (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -9,7 +9,6 @@ def create_table_cliente(*args,**kwargs):
         dirección TEXT NOT NULL;
         teléfono TEXT NOT NULL;
         email TEXT NOT NULL;
-
     )
     '''
     cursor.execute(query)
@@ -18,8 +17,7 @@ def create_table_cliente(*args,**kwargs):
 
 
 def create_table_pago(*args,**kwargs):
-    connection = kwargs.pop('connection')
-    cursor = connection.cursor()
+    conn = kwargs.pop("database.db")
     query  = '''
     CREATE TABLE IF NOT EXISTS cliente (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,15 +25,14 @@ def create_table_pago(*args,**kwargs):
         modelo TEXT NOT NULL;
     )
     '''
-    cursor.execute(query)
+    cursor.execute(create_table_pago)
     result = cursor.fetchone()
     return result
 
-
 def create_table_vehiculo(*args,**kwargs):
     connection = kwargs.pop('connection')
-    cursor = connection.cursor()
-    query  = '''
+    conn = kwargs.pop("database.db")
+    create_table_vehiculo  = '''
     CREATE TABLE IF NOT EXISTS vehiculo (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         modelo TEXT NOT NULL;
@@ -45,10 +42,34 @@ def create_table_vehiculo(*args,**kwargs):
     '''
     cursor.execute(create_table_vehiculo)
     result = cursor.fetchone()
-    return result 
+    return result
 
+def create_table_reserva(*args,**kwargs):
+    conn = kwargs.pop("database.db")
+    cursor = conn.cursor()
+    query  = '''
+        cliente_id INTEGER,
+        FOREIGN KEY (vehiculo_id) REFERENCES vehiculo(id)
+        fechaRecogida DATE,
+        fechaEntrega DATE,
+        pago_id INTEGER,
+        FOREIGN KEY (pago_id) REFERENCES pago(id)
+    )
+    '''
     cursor.execute(query)
     result = cursor.fetchone()
     return result
+
+
+
+
+
+
+
+
+
+
+
+
 
 
